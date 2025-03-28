5f562f1eb0e70059ab592f64b1c0381c02415b87
Miguel Angel Estrada <99843269+ShamisenIsCool@users.noreply.github.com>
2025-03-28 17:52:14 +0000

    All websites have been added. Only publications are pending.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ websites_names = scrapper.get_webnames()
 
 #scrapper.get_all_papers()
 #scrapper.get_all_reports()
-scrapper.get_all_speeches()
+#scrapper.get_all_speeches()
 
 @app.route('/')
 def main(): 
